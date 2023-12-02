bc2c3846c56c10cbd3b82c3948d7f4ab4e987534
Othman Al Ghandour <othmanghandour51@gmail.com>
2023-12-02 22:29:19 +0300

    add linked list implementation

---
+++ LinkedList/main.py
@@ -0,0 +1,11 @@
+# A linked list is a linear data structure used in computer science to organize and store data. Unlike arrays, which use contiguous memory locations to store elements, a linked list consists of nodes where each node contains data and a reference (or link) to the next node in the sequence.
+
+# Basic Operations : 
+#     - Insertion
+#     - Searching
+#     - Deletion
+#     - Creating a Linked List
+#     - Deleting a Linked List
+#     - Printing a Linked List
+
+
