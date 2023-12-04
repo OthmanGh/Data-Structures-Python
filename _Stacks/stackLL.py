d509375c2b826becef781a0287aea83b290fa8f3
Othman Al Ghandour <othmanghandour51@gmail.com>
2023-12-05 00:47:46 +0300

    change var names to add more clarity

--- _Stacks/stackLL.py
+++ _Stacks/stackLL.py
@@ -11,28 +11,28 @@ class Node:
 # Stack class represents a stack implemented using a linked list
 class Stack:
     def __init__(self):
-        # Initialize an empty stack with the head set to None
-        self.head = None
+        # Initialize an empty stack with the top set to None
+        self.top = None
 
     def push(self, value):
         # Push (add) a new element onto the stack
-        # Create a new node with the given value and update the head
-        n = Node(value, self.head)
-        self.head = n
+        # Create a new node with the given value and update the top
+        n = Node(value, self.top)
+        self.top = n
 
     def pop(self):
         # Pop (remove) the top element from the stack
         if not self.empty():
-            # Update the head to point to the next node, effectively removing the top element
-            self.head = self.head.next
+            # Update the top to point to the next node, effectively removing the top element
+            self.top = self.top.next
 
     def empty(self):
         # Check if the stack is empty
-        return self.head == None
+        return self.top == None
     
-    def top(self):
+    def peek(self):
         # Return the data of the top element in the stack
-        return self.head.data
+        return self.top.data
     
 # Create a stack object
 s = Stack()
@@ -45,13 +45,13 @@ s.push(30)
 s.push(50)
 s.push(70)
 
-# print(s.top())
+# print(s.peek())
 # s.pop()
-# print(s.top())
+# print(s.peek())
 
 # Print the elements at the top of the stack in a LIFO order
 while not s.empty():
-    print(s.top(), end=" -> ")
+    print(s.peek(), end=" -> ")
     s.pop()
 print()
 
