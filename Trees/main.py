547552e82203782312881686fa92559a2fd26b52
Othman Al Ghandour <othmanghandour51@gmail.com>
2023-12-12 23:38:19 +0300

    implement node class for data items inside tree

--- Trees/main.py
+++ Trees/main.py
@@ -1,3 +1,8 @@
 #A binary tree with the property that the value of each node's left child is less than or equal to the node's value, and the value of the right child is greater than or equal to the node's value.
 
 
+class Node:
+    def __init__(self, value, left, right):
+        self.value = value
+        self.right = right
+        self.left = left
