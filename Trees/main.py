c766368ec8377fe127b212a7f4189e07d941581b
Othman Al Ghandour <othmanghandour51@gmail.com>
2023-12-12 23:37:31 +0300

    add tree data structure folder

---
+++ Trees/main.py
@@ -0,0 +1,3 @@
+#A binary tree with the property that the value of each node's left child is less than or equal to the node's value, and the value of the right child is greater than or equal to the node's value.
+
+
