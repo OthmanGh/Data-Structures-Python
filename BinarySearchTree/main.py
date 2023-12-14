d410e556d79882368444a69d11dba978c050bff9
Othman Al Ghandour <othmanghandour51@gmail.com>
2023-12-14 14:06:24 +0300

    create node class for vertices in the tree

--- BinarySearchTree/main.py
+++ BinarySearchTree/main.py
@@ -1,1 +1,9 @@
 #A binary tree with the property that the value of each node's left child is less than or equal to the node's value, and the value of the right child is greater than or equal to the node's value.
+
+class Node:
+    def __init__(self, value, parent = None, left = None, right = None):
+        self.data = value
+        self.parent = parent
+        self.left = left
+        self.right = right
+
