e1dcf55a75514a94f590e3ad00749b2a128c7586
Othman Al Ghandour <othmanghandour51@gmail.com>
2023-12-14 14:19:26 +0300

    implement add method

--- BinarySearchTree/main.py
+++ BinarySearchTree/main.py
@@ -10,3 +10,33 @@ class Node:
 class BST:
     def __init__(self):
         self.root = None
+
+    
+    def add(self, value):
+        n = Node(value)
+
+        if self.root == None:
+            self.root = n
+
+        else:
+            current = self.root
+            self.add_helper(n, current)
+
+    def add_helper(self, n, current):
+        if current.value > n.value:
+            if current.left == None:
+                current.left = n
+                n.parent = current
+                return 
+            else:
+                return self.add_helper(n, current.left)
+            
+        else:
+            if current.right == None:
+                current.right = n
+                n.parent = current
+                return
+            
+            else:
+                return self.add_helper(n, current.right)
+            
